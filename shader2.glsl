c51ea1df7f6c08afe7dcd2e73b59f4a75a95cedb
EternalLite <cdavid.tran@gmail.com>
2014-01-18 20:20:30 -0500

    Switching back to VTK base

--- shader2.glsl
+++
@@ -1,8 +0,0 @@
-#version 120
-//#extension GL_EXT_geometry_shader4 : enable
-
-void main(void)  
-{     
-   gl_TexCoord[0] = gl_MultiTexCoord0;   
-   gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;  
-}
